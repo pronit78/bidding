fbeac8803fb2eaa9bb2993f50dd6512bbdc222de
pronit <pronit78@gmail.com>
2017-03-18 03:39:50 +0530

    Initial commit

---
+++ src/main/java/com/socialharazi/DressBiddingApplication.java
@@ -0,0 +1,16 @@
+package com.socialharazi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class DressBiddingApplication {
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DressBiddingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/socialharazi/DressBiddingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.socialharazi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DressBiddingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
